b9557d40dab541a88a6e9ef03ce5bdcb40cc3b52
Hansol Yoon <yoondev3434@gmail.com>
2022-08-29 16:08:51 +0900

    footer...

--- components/footer/app-footer.js
+++ components/footer/app-footer.js
@@ -1,7 +1,94 @@
 import React from "react";
+import styles from "../../styles/components/app-footer.module.scss";
+
+const __TABS__ = [
+    {
+        key: "about_us",
+        i18nKey: "tab.about_us",
+        path: "/",
+        onClick: () => {},
+    },
+    {
+        key: "terms_of_service",
+        i18nKey: "tab.terms_of_service",
+        path: "/terms_of_service",
+        onClick: () => {},
+    },
+    {
+        key: "privacy_policy",
+        i18nKey: "tab.privacy_policy",
+        path: "/privacy_policy",
+        onClick: () => {},
+    },
+    {
+        key: "service_center",
+        i18nKey: "tab.service_center",
+        path: "/service_center",
+        onClick: () => {},
+    },
+];
+
+const __SNS__ = [
+    {
+        key: "github",
+        i18nKey: "sns.github",
+        onClick: () => {
+            window.open("https://github.com/DAMOA7989", "_blank");
+        },
+    },
+    {
+        key: "medium",
+        i18nKey: "sns.medium",
+        onClick: () => {
+            window.alert("We are ready.");
+        },
+    },
+    {
+        key: "facebook",
+        i18nKey: "sns.facebook",
+        onClick: () => {
+            window.alert("We are ready.");
+        },
+    },
+    {
+        key: "instagram",
+        i18nKey: "sns.instagram",
+        onClick: () => {
+            window.alert("We are ready.");
+        },
+    },
+];
 
 const AppFooter = () => {
-    return <footer>DAMOA7989</footer>;
+    return (
+        <footer className={styles.container}>
+            <ul className={styles.tabs_list}>
+                {__TABS__.map((tab, idx) => (
+                    <li key={tab.key} className={styles.tabs_list_item}>
+                        {tab.i18nKey}
+                    </li>
+                ))}
+            </ul>
+            <ul className={styles.sns_list}>
+                {__SNS__.map((sns, idx) => (
+                    <li key={sns.key}>
+                        <div
+                            className={styles[`${sns.key}_icon`]}
+                            onClick={sns.onClick}
+                        ></div>
+                    </li>
+                ))}
+            </ul>
+            <ul className={styles.info_list}>
+                <li className={styles.info_list_item}>
+                    <p>address: test</p>
+                </li>
+                <li className={styles.info_list_item}>
+                    <p>damoa7989</p>
+                </li>
+            </ul>
+        </footer>
+    );
 };
 
 export default AppFooter;
