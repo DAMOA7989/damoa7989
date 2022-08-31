cbd3a8ecac600866bbad758dd7909604123d11c8
Hansol Yoon <yoondev3434@gmail.com>
2022-08-31 17:55:24 +0900

    contact page is added.

--- pages/[locale]/contact.js
+++ pages/[locale]/contact.js
@@ -1,18 +1,53 @@
 import React from "react";
+import styles from "styles/pages/contact.module.scss";
 import Head from "next/head";
 import AppLayout from "../../components/layout/app-layout";
 import { getStaticPaths, makeStaticProps } from "../../lib/getStatic";
+import { useTranslation, Trans } from "next-i18next";
 
 const getStaticProps = makeStaticProps(["common"]);
 export { getStaticPaths, getStaticProps };
 
 const Contact = () => {
+    const { t } = useTranslation();
+
+    React.useEffect(() => {
+        const map = new naver.maps.Map("map", {
+            center: new naver.maps.LatLng(37.5075512, 127.060561),
+            zoom: 17,
+        });
+    }, []);
+
     return (
         <AppLayout>
             <Head>
-                <title>DAMOA7989 Contact</title>
+                <title>DAMOA7989: Contact</title>
             </Head>
-            contact
+            <div className={styles.container}>
+                <article className={styles.head_quarter}>
+                    <h5 className={styles.title}>
+                        {t("title.contact.head_quarter")}
+                    </h5>
+                    <div className={styles.map_container}>
+                        <div
+                            id="map"
+                            className={styles.map}
+                            style={{ width: "100%", height: "100%" }}
+                        ></div>
+                    </div>
+                    <div className={styles.address}>
+                        <span>{t("title.contact.address")}</span>
+                        <Trans parent="span" i18nKey={"text.contact.address"} />
+                    </div>
+                    <div className={styles.phone_number}>
+                        <span>{t("title.contact.phone_number")}</span>
+                        <Trans
+                            parent="span"
+                            i18nKey={"text.contact.phone_number"}
+                        />
+                    </div>
+                </article>
+            </div>
         </AppLayout>
     );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,18 @@ import "../styles/globals.css";
 import "../styles/reset.css";
 import "styles/global.scss";
 import { appWithTranslation } from "next-i18next";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Script
+                strategy="beforeInteractive"
+                src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_WEB_DYNAMIC_MAP_CLIENT_ID}`}
+            ></Script>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default appWithTranslation(MyApp);
